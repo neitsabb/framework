49dbba701ef92125a45256801a4616c4bb6a82df
BastienVdp <bastienpp@hotmail.com>
2024-04-15 00:11:07 +0200

    Database Abstraction Layer

---
+++ app/core/ConnectionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Core;
+
+use Doctrine\DBAL\Connection;
+
+class ConnectionFactory
+{
+	private Config $config;
+
+	public string $defaultConnection;
+
+	public function __construct(Config $config)
+	{
+		$this->config = $config;
+		$this->defaultConnection = $this->config->get('database.default');
+	}
+
+	public function make(): Connection
+	{
+		$defaultConnection = $this->config->get('database.default');
+		$connectionParams = $this->config->get("database.connections.{$defaultConnection}");
+
+		return \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+	}
+}
--- config/database.php
+++ config/database.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 return [
 
@@ -24,24 +24,24 @@ return [
 	 */
 	'connections' => [
 		'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'localhost'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+			'driver' => 'pdo_mysql',
+			'url' => env('DATABASE_URL'),
+			'host' => env('DB_HOST', '127.0.0.1'),
+			'port' => env('DB_PORT', '3306'),
+			'database' => env('DB_DATABASE', 'localhost'),
+			'username' => env('DB_USERNAME', 'root'),
+			'password' => env('DB_PASSWORD', ''),
+			'unix_socket' => env('DB_SOCKET', ''),
+			'charset' => 'utf8mb4',
+			'collation' => 'utf8mb4_unicode_ci',
+			'prefix' => '',
+			'prefix_indexes' => true,
+			'strict' => true,
+			'engine' => null,
+			'options' => extension_loaded('pdo_mysql') ? array_filter([
+				PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+			]) : [],
+		],
 	],
 
 	/**
