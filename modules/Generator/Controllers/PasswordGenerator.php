757c570082daa33f78188db075667075881f607b
BastienVdp <bastienpp@hotmail.com>
2024-04-15 00:11:54 +0200

    Little update for example app controller

--- modules/Generator/Controllers/PasswordGenerator.php
+++ modules/Generator/Controllers/PasswordGenerator.php
@@ -5,21 +5,15 @@ namespace Modules\Generator\Controllers;
 use App\Core\Request;
 use App\Core\Response;
 use App\Core\Controller;
-use Modules\Formatter\Contracts\FormatterInterface;
+use Modules\Generator\Services\PasswordFormatter;
 
 class PasswordGenerator extends Controller
 {
-	private FormatterInterface $formatter;
+	private PasswordFormatter $formatter;
 
-	public array $views;
-
-	public function __construct(FormatterInterface $formatter)
+	public function __construct(PasswordFormatter $formatter)
 	{
 		$this->formatter = $formatter;
-
-		$this->views = [
-			'generator'
-		];
 	}
 
 	public static function routes()
@@ -40,44 +34,12 @@ class PasswordGenerator extends Controller
 
 	public function index()
 	{
-		return $this->render('generator');
+
+		return new Response('coucou');
 	}
 
 	public function generate(Request $request, Response $response)
 	{
-		$errors = [];
-
-		if (!$request->getBody('length')) {
-			$errors['length'] = 'La longueur est requise.';
-		}
-
-		if (!$request->getBody('lowercase') && !$request->getBody('uppercase') && !$request->getBody('digits') && !$request->getBody('specialchars')) {
-			$errors['options'] = 'Au moins une option doit être cochée.';
-		}
-
-		if (empty($errors)) {
-			$length = $request->getBody('length');
-			$lowercase = $request->getBody('lowercase');
-			$uppercase = $request->getBody('uppercase');
-			$digits = $request->getBody('digits');
-			$specialchars = $request->getBody('specialchars');
-
-			$password = $this->formatter
-				->format(value: [
-					'length' => $length,
-					'lowercase' => $lowercase,
-					'uppercase' => $uppercase,
-					'digits' => $digits,
-					'specialchars' => $specialchars
-				]);
-
-			return $this->render('generator', [
-				'password' => $password,
-				'request' => $request->all()
-			]);
-		} else return $this->render('generator', [
-			'errors' => $errors,
-			'request' => $request->all()
-		]);
+		return 'Hello! ';
 	}
 }
